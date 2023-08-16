39f12535029461b2c8c1ff222a399fd6003a7d2e
GuilhermeHenriquinho <99337293+GuilhermeHenriquinho@users.noreply.github.com>
2023-08-15 21:03:47 -0300

    finishing relationship screens

--- gensource/src/gensource/view/TelaAnotacao.java
+++ gensource/src/gensource/view/TelaAnotacao.java
@@ -23,6 +23,7 @@ import gensource.model.enumAndModel.Anotacao;
 import gensource.model.enumAndModel.AnotacaoModel;
 import gensource.view.anotations.TelaColumn;
 import gensource.view.anotations.TelaGeneratedValue;
+import gensource.view.anotations.TelaRelacionamentos;
 
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -38,12 +39,12 @@ public class TelaAnotacao extends JDialog {
 
 	private ObjectTableModel<AnotacaoModel> model = null;
 	private ObjectTableModel<AnotacaoString> modelString = null;
-    private List<AnotacaoModel> anotacoes = new ArrayList<>();
-    private List<AnotacaoString> anotacoesString = new ArrayList<>();
+	private List<AnotacaoModel> anotacoes = new ArrayList<>();
+	private List<AnotacaoString> anotacoesString = new ArrayList<>();
 	private JTable table;
 	private JTable table2;
-    private Atributo atributo;
-    private Atributo retorno;
+	private Atributo atributo;
+	private Atributo retorno;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -68,27 +69,28 @@ public class TelaAnotacao extends JDialog {
 		this.getContentPane().setBackground(new Color(255, 255, 255));
 		this.setBounds(100, 100, 532, 509);
 		this.getContentPane().setLayout(null);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(10, 88, 496, 118);
 		this.getContentPane().add(scrollPane);
-		
+
 		table = new JTable();
 		scrollPane.setViewportView(table);
-		
+
 		JButton btnNewButton = new JButton("Concluir");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+
 				List<AnotacaoString> anotacoes = modelString.getData();
-				if(Objects.nonNull(anotacoes) && !anotacoes.isEmpty()) {
+				if (Objects.nonNull(anotacoes) && !anotacoes.isEmpty()) {
 					List<String> anotacoesString = new ArrayList<>();
-					for(AnotacaoString an : anotacoes) {
+					for (AnotacaoString an : anotacoes) {
 						anotacoesString.add(an.getAnotacao());
 					}
 					getAtributo().setAnotacao(anotacoesString);
 					setRetorno(getAtributo());
-					JOptionPane.showMessageDialog(null, "Anotações para o Atributo \""+getAtributo().getNomeAtributo()+"\" salvas com Sucesso!");
+					JOptionPane.showMessageDialog(null, "Anotações para o Atributo \"" + getAtributo().getNomeAtributo()
+							+ "\" salvas com Sucesso!");
 					dispose();
 				} else {
 					JOptionPane.showMessageDialog(null, "Por favor adicione anotações para o atributo.");
@@ -97,7 +99,7 @@ public class TelaAnotacao extends JDialog {
 		});
 		btnNewButton.setBounds(398, 424, 108, 35);
 		this.getContentPane().add(btnNewButton);
-		
+
 		JButton btnCancelar = new JButton("Cancelar");
 		btnCancelar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -106,96 +108,114 @@ public class TelaAnotacao extends JDialog {
 		});
 		btnCancelar.setBounds(280, 424, 108, 35);
 		this.getContentPane().add(btnCancelar);
-		
+
 		JButton btnAdicionar = new JButton("Adicionar");
 		btnAdicionar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+
 				AnotacaoModel am = model.getValue(table.getSelectedRow());
-				
-				if(Objects.nonNull(am)) {
-					//Column
-					if(am.getTitulo().contains("Column")) {
+
+				if (Objects.nonNull(am)) {
+					
+					// Column
+					if (am.getTitulo().contains("Column")) {
 						TelaColumn screen = new TelaColumn();
 						screen.setVisible(true);
-						if(Objects.nonNull(screen.getAnotacao()) && screen.getAnotacao().length() > 0) {
+						if (Objects.nonNull(screen.getAnotacao()) && screen.getAnotacao().length() > 0) {
 							AnotacaoString anotacao = new AnotacaoString();
 							anotacao.setAnotacao(screen.getAnotacao());
-							anotacoesString.add(anotacao);
-							carregaTableString(anotacoesString);
-						}
-					}
-					
-					//Relacionamentos
-//					if() {
-//						
-//					}
-					
-					//GeneratedValue
-					if(getAtributo().getNomeAtributo().equals("id") && am.getTitulo().contains("GeneratedValue")) {
-						TelaGeneratedValue screen = new TelaGeneratedValue();
-						screen.setVisible(true);
-						if(Objects.nonNull(screen.getAnotacao()) && screen.getAnotacao().length() > 0) {
-							AnotacaoString anotacao = new AnotacaoString();
-							anotacao.setAnotacao(screen.getAnotacao());
-							anotacoesString.add(anotacao);
-							
 							for (int i = 0; i < anotacoesString.size(); i++) {
-								if (anotacoesString.get(i).getAnotacao().contains("GeneratedValue")) {
+								if (anotacoesString.get(i).getAnotacao().contains("@Column")) {
 									anotacoesString.remove(i);
-
-									for (int j = 0; j < getAtributo().getAnotacao().size(); j++) {
-										if (getAtributo().getAnotacao().get(j).contains("GeneratedValue")) {
-											getAtributo().getAnotacao().remove(j);
-											break;
-										}
-									}
 									break;
 								}
 							}
-							
+							anotacoesString.add(anotacao);
 							carregaTableString(anotacoesString);
 						}
 					}
 					
-					
-					
-					
+					//Transient
+					if(am.getTitulo().contains("Transient")) {
+				        int resposta = JOptionPane.showConfirmDialog(null, "Deseja confirmar a adição da anotação Transient?", "Confirmação", JOptionPane.YES_NO_OPTION);
+				        
+				        if (resposta == JOptionPane.YES_OPTION) {
+				            AnotacaoString anotacao = new AnotacaoString();
+				            anotacao.setAnotacao("@Transient");
+				            anotacoesString.add(anotacao);
+				            carregaTableString(anotacoesString);
+				            
+				            JOptionPane.showMessageDialog(null, "Anotação adicionada com Sucesso!");
+				        }
+					}
+
+					// Relacionamentos
+					if (getAtributo().getIsRelacionamento() && am.getTitulo().contains("OneToMany")
+							|| am.getTitulo().contains("ManyToOne") || am.getTitulo().contains("OneToOne")) {
+						TelaRelacionamentos screen = new TelaRelacionamentos();
+						screen.setRelation("@" + am.getTitulo());
+						screen.setVisible(true);
+						if (Objects.nonNull(screen.getAnotacao()) && screen.getAnotacao().length() > 0) {
+							AnotacaoString anotacao = new AnotacaoString();
+							anotacao.setAnotacao(screen.getAnotacao());
+							for (int i = 0; i < anotacoesString.size(); i++) {
+								if (anotacoesString.get(i).getAnotacao().contains("@" + am.getTitulo())) {
+									anotacoesString.remove(i);
+									break;
+								}
+							}
+							anotacoesString.add(anotacao);
+							carregaTableString(anotacoesString);
+						}
+					}
+
+					// GeneratedValue
+					if (getAtributo().getNomeAtributo().equals("id") && am.getTitulo().contains("GeneratedValue")) {
+						TelaGeneratedValue screen = new TelaGeneratedValue();
+						screen.setVisible(true);
+						if (Objects.nonNull(screen.getAnotacao()) && screen.getAnotacao().length() > 0) {
+							AnotacaoString anotacao = new AnotacaoString();
+							anotacao.setAnotacao(screen.getAnotacao());
+							for (int i = 0; i < anotacoesString.size(); i++) {
+								if (anotacoesString.get(i).getAnotacao().contains("@GeneratedValue")) {
+									anotacoesString.remove(i);
+									break;
+
+								}
+							}
+							anotacoesString.add(anotacao);
+							carregaTableString(anotacoesString);
+						}
+					}
 				}
 			}
 		});
 		btnAdicionar.setBounds(417, 217, 89, 23);
 		this.getContentPane().add(btnAdicionar);
-		
+
 		JScrollPane scrollPane_1 = new JScrollPane();
 		scrollPane_1.setBounds(10, 268, 496, 118);
 		this.getContentPane().add(scrollPane_1);
-		
+
 		JLabel lblNewLabel = new JLabel("Anota\u00E7\u00F5es disponiveis");
 		lblNewLabel.setForeground(new Color(0, 0, 0));
 		lblNewLabel.setBackground(new Color(119, 136, 153));
 		lblNewLabel.setBounds(10, 72, 496, 14);
 		this.getContentPane().add(lblNewLabel);
-		
+
 		JLabel lblAnotaesUtilizadas = new JLabel("Anota\u00E7\u00F5es Utilizadas");
 		lblAnotaesUtilizadas.setBounds(10, 251, 496, 14);
 		this.getContentPane().add(lblAnotaesUtilizadas);
-		
+
 		table2 = new JTable();
 		scrollPane_1.setViewportView(table2);
-		
-		JLabel lblNewLabel_1 = new JLabel("2 Cliques em cima da Anota\u00E7\u00E3o para editar - Apertar DEL com a anota\u00E7\u00E3o selecionada para deletar");
-		lblNewLabel_1.setForeground(new Color(255, 0, 0));
-		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1.setBounds(10, 388, 496, 14);
-		this.getContentPane().add(lblNewLabel_1);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("Anota\u00E7\u00F5es para o Campo:");
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblNewLabel_2.setBounds(10, 26, 312, 23);
 		this.getContentPane().add(lblNewLabel_2);
-		
+
 		JLabel lbNomeCampo = new JLabel("");
 		lbNomeCampo.setForeground(new Color(255, 165, 0));
 		lbNomeCampo.setFont(new Font("Tahoma", Font.PLAIN, 16));
@@ -212,16 +232,16 @@ public class TelaAnotacao extends JDialog {
 		carregaTable();
 		carregaTableString(anotacoesString);
 	}
-	
+
 	public void enumToObject() {
-        Anotacao[] anotacoes = Anotacao.values();
-        for (int i = 0; i < anotacoes.length; i++) {
-        	AnotacaoModel anotacaoModel = new AnotacaoModel();
-        	anotacaoModel.setId(i+1);
-        	anotacaoModel.setTitulo(anotacoes[i].getTitulo());
-        	anotacaoModel.setExplicacao(anotacoes[i].getExplicacao());
-        	this.anotacoes.add(anotacaoModel);
-        }
+		Anotacao[] anotacoes = Anotacao.values();
+		for (int i = 0; i < anotacoes.length; i++) {
+			AnotacaoModel anotacaoModel = new AnotacaoModel();
+			anotacaoModel.setId(i + 1);
+			anotacaoModel.setTitulo(anotacoes[i].getTitulo());
+			anotacaoModel.setExplicacao(anotacoes[i].getExplicacao());
+			this.anotacoes.add(anotacaoModel);
+		}
 	}
 
 	public List<AnotacaoModel> getAnotacoes() {
@@ -239,15 +259,15 @@ public class TelaAnotacao extends JDialog {
 	public void setAtributo(Atributo atributo) {
 		this.atributo = atributo;
 	}
-	
+
 	public void carregaTable() {
 		AnnotationResolver resolver = new AnnotationResolver(AnotacaoModel.class);
 		model = new ObjectTableModel<AnotacaoModel>(resolver, "titulo:Titulo,explicacao:Explicação");
 		model.setData(anotacoes);
 		table.setModel(model);
-	    table.getColumnModel().getColumn(0).setPreferredWidth(50);
+		table.getColumnModel().getColumn(0).setPreferredWidth(50);
 	}
-	
+
 	public void carregaTableString(List<AnotacaoString> anotacoes) {
 		AnnotationResolver resolver = new AnnotationResolver(AnotacaoString.class);
 		modelString = new ObjectTableModel<AnotacaoString>(resolver, "anotacao:Anotação");
@@ -270,5 +290,5 @@ public class TelaAnotacao extends JDialog {
 	public void setAnotacoesString(List<AnotacaoString> anotacoesString) {
 		this.anotacoesString = anotacoesString;
 	}
-	
+
 }
--- gensource/src/gensource/view/anotations/TelaRelacionamentos.java
+++ gensource/src/gensource/view/anotations/TelaRelacionamentos.java
@@ -4,8 +4,35 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.SwingConstants;
+import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
+import javax.swing.border.EtchedBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.DefaultComboBoxModel;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-public class TelaRelacionamentos extends JFrame{
+public class TelaRelacionamentos extends JDialog {
+	private JLabel lblMappedby;
+	private JComboBox cbCascade;
+	private JLabel lblFetch;
+	private JComboBox cbFetch;
+	private JCheckBox ckOrphan;
+	private JButton btnSalvar;
+	private JButton btnCancelar;
+	private JLabel lblField;
+	private JTextField txtMappedBy;
+	private String relation = ""; 
+	private String anotacao;
 
 	/**
 	 * Launch the application.
@@ -34,10 +61,152 @@ public class TelaRelacionamentos extends JFrame{
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowOpened(WindowEvent e) {
+				setTitle(getRelation());
+			}
+		});
+		setModal(true);
 		getContentPane().setBackground(new Color(255, 255, 255));
-		setTitle("Relacionamento");
-		setBounds(100, 100, 385, 300);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle(getRelation());
+		setBounds(100, 100, 262, 251);
+		getContentPane().setLayout(null);
+		getContentPane().add(getBtnSalvar());
+		getContentPane().add(getBtnCancelar());
+		getContentPane().add(getCkOrphan());
+		getContentPane().add(getCbCascade());
+		getContentPane().add(getLblMappedby());
+		getContentPane().add(getLblFetch());
+		getContentPane().add(getCbFetch());
+		getContentPane().add(getTxtMappedBy());
+		getContentPane().add(getLblField());
+	}
+	private JLabel getLblMappedby() {
+		if (lblMappedby == null) {
+			lblMappedby = new JLabel("Cascade:");
+			lblMappedby.setBounds(10, 98, 71, 14);
+			lblMappedby.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		}
+		return lblMappedby;
+	}
+	private JComboBox getCbCascade() {
+		if (cbCascade == null) {
+			cbCascade = new JComboBox();
+			cbCascade.setBounds(90, 93, 130, 26);
+			cbCascade.setModel(new DefaultComboBoxModel(new String[] {"", "ALL", "PERSIST", "MERGE", "REMOVE", "DETACH", "REFRESH"}));
+		}
+		return cbCascade;
+	}
+	private JLabel getLblFetch() {
+		if (lblFetch == null) {
+			lblFetch = new JLabel("Fetch:");
+			lblFetch.setBounds(10, 65, 71, 14);
+			lblFetch.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		}
+		return lblFetch;
+	}
+	private JComboBox getCbFetch() {
+		if (cbFetch == null) {
+			cbFetch = new JComboBox();
+			cbFetch.setBounds(90, 60, 130, 26);
+			cbFetch.setModel(new DefaultComboBoxModel(new String[] {"", "EAGER", "LAZY"}));
+		}
+		return cbFetch;
+	}
+	private JCheckBox getCkOrphan() {
+		if (ckOrphan == null) {
+			ckOrphan = new JCheckBox("OrphanRemoval");
+			ckOrphan.setBounds(120, 126, 103, 23);
+			ckOrphan.setHorizontalAlignment(SwingConstants.RIGHT);
+		}
+		return ckOrphan;
+	}
+	private JButton getBtnSalvar() {
+		if (btnSalvar == null) {
+			btnSalvar = new JButton("Salvar");
+			btnSalvar.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					String anot = getRelation() + "(";
+					String fetch = cbFetch.getSelectedItem().toString();
+					String cascade = cbCascade.getSelectedItem().toString();
+					String field = txtMappedBy.getText();
+					
+					if(field.length() > 0) {
+						anot += "mappedBy = \"" + field + "\"";
+					}
+					
+					if(cascade.length() > 0) {
+						if (anot.endsWith("\"") ) {
+							anot += ", ";
+						}
+						anot += "cascade = CascadeType." + cascade;
+					}
+					
+					if(fetch.length() > 0) {
+						if (anot.endsWith("\"") || !anot.endsWith("(")) {
+							anot += ", ";
+						}
+						anot += "fetch = FetchType." + fetch;
+					}
+					
+					
+					if (anot.endsWith("\"") || !anot.endsWith("(")) {
+						anot += ", ";
+					}
+					anot += "orphanRemoval = "+ (ckOrphan.isSelected() ? "true" : "false") +")";
+					
+					setAnotacao(anot);
+					dispose();
+				}
+			});
+			btnSalvar.setBounds(121, 156, 89, 39);
+		}
+		return btnSalvar;
+	}
+	private JButton getBtnCancelar() {
+		if (btnCancelar == null) {
+			btnCancelar = new JButton("Cancelar");
+			btnCancelar.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					dispose();
+				}
+			});
+			btnCancelar.setBounds(22, 156, 89, 39);
+		}
+		return btnCancelar;
+	}
+	private JLabel getLblField() {
+		if (lblField == null) {
+			lblField = new JLabel("MappedBy:");
+			lblField.setBounds(10, 34, 81, 14);
+			lblField.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		}
+		return lblField;
+	}
+	private JTextField getTxtMappedBy() {
+		if (txtMappedBy == null) {
+			txtMappedBy = new JTextField();
+			txtMappedBy.setBounds(90, 28, 130, 26);
+			txtMappedBy.setColumns(10);
+		}
+		return txtMappedBy;
 	}
 
+	public String getRelation() {
+		return relation;
+	}
+
+	public void setRelation(String relation) {
+		this.relation = relation;
+	}
+
+	public String getAnotacao() {
+		return anotacao;
+	}
+
+	public void setAnotacao(String anotacao) {
+		this.anotacao = anotacao;
+	}
+	
 }
